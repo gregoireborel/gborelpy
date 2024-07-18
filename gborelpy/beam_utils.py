2eed61c7b438ae01b2cff494ba9a86c7bb100565
Gregoire Borel <gregoire.borel@neoxia.com>
2024-07-18 14:50:15 +0200

    Update GeneratePath and add UpsertQueryToBQ

--- gborelpy/beam_utils.py
+++ gborelpy/beam_utils.py
@@ -2,6 +2,7 @@ import json
 import logging
 
 import apache_beam as beam
+from google.cloud import bigquery
 
 
 class ParseJSON(beam.DoFn):
@@ -21,22 +22,61 @@ class GeneratePath(beam.DoFn):
 
     bucket : str
         The name of the GCS bucket.
-    input_date : ValueProvider
-        A ValueProvider for the input date.
+    input_date : str
+        A string for the input date.
     event_types : list
         A list of predefined event types for generating paths.
 
     """
 
-    def __init__(self, bucket, input_date, event_types):
+    def __init__(self, bucket, files_prefix, input_date):
         self.bucket = bucket
+        self.files_prefix = files_prefix
         self.input_date = input_date
-        self.event_types = event_types
 
     def process(self, element):
         """
         Generates a GCS path for each event type and yields it.
         """
-        for event_type in self.event_types:
-            yield f"gs://{self.bucket}/{self.event_type}/{self.input_date.get()}**/**"
+        path = (
+            f"gs://{self.bucket}/{self.files_prefix}/**"
+            if self.files_prefix
+            else f"gs://{self.bucket}"
+        )
+        final_path = (
+            f"{path}/{self.input_date.get()}**/**" if self.input_date else f"{path}/**"
+        )
+        yield final_path
 
+
+class UpsertQueryToBQ(beam.DoFn):
+    def __init__(self, project_id, dataset, table, query):
+        self.project_id = project_id
+        self.dataset = dataset
+        self.table = table
+        self.QUERY = query
+
+    def setup(self):
+        self.bigquery_client = bigquery.Client(project=self.project_id)
+
+    def process(self, element):
+        try:
+            logging.info(
+                "Executing MERGE operation for {}.{}.{} table.".format(
+                    self.project_id, self.dataset, self.table
+                )
+            )
+            query_job = self.bigquery_client.query(self.QUERY)
+            query_job.result()
+            logging.info(
+                "MERGE operation completed for {}.{}.{} table.".format(
+                    self.project_id, self.dataset, self.table
+                )
+            )
+
+        except Exception as e:
+            logging.error(
+                "Error during MERGE operation for {}.{}.{} table: {}".format(
+                    self.project_id, self.dataset, self.table, str(e)
+                )
+            )
